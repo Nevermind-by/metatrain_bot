348ff731f989fa30ac94e6753033c61246eadda2
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-08-27 22:09:29 +0300

    feat: connect progress router

--- app/bot/main.py
+++ app/bot/main.py
@@ -6,6 +6,7 @@ from app.config.settings import settings
 from app.database.connection import init_database
 from app.handlers.food import router as food_router
 from app.handlers.profile import router as profile_router
+from app.handlers.progress import router as progress_router
 from app.handlers.start import router as start_router
 
 
@@ -15,6 +16,7 @@ async def main() -> None:
     dispatcher.include_router(start_router)
     dispatcher.include_router(profile_router)
     dispatcher.include_router(food_router)
+    dispatcher.include_router(progress_router)
 
     await init_database()
     print("MetaTrain initialization completed.")
