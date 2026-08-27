84d81e5d7d6cd1eb267ade4f65a7b6b188e5b1a6
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-08-27 22:26:57 +0300

    feat: add product catalog states

--- app/bot/states.py
+++ app/bot/states.py
@@ -2,28 +2,16 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class ProfileStates(StatesGroup):
-    gender = State()
-    age = State()
-    height = State()
-    weight = State()
-    activity_level = State()
-    goal = State()
-    edit_gender = State()
-    edit_age = State()
-    edit_height = State()
-    edit_weight = State()
-    edit_activity_level = State()
-    edit_goal = State()
+    gender = State(); age = State(); height = State(); weight = State(); activity_level = State(); goal = State()
+    edit_gender = State(); edit_age = State(); edit_height = State(); edit_weight = State(); edit_activity_level = State(); edit_goal = State()
 
 
 class FoodStates(StatesGroup):
-    meal = State()
-    product_name = State()
-    grams = State()
-    calories = State()
-    protein = State()
-    fat = State()
-    carbohydrates = State()
+    meal = State(); product_name = State(); grams = State(); calories = State(); protein = State(); fat = State(); carbohydrates = State()
+
+
+class ProductStates(StatesGroup):
+    name = State(); calories = State(); protein = State(); fat = State(); carbohydrates = State()
 
 
 class WeightStates(StatesGroup):
@@ -31,7 +19,4 @@ class WeightStates(StatesGroup):
 
 
 class WorkoutStates(StatesGroup):
-    name = State()
-    duration = State()
-    calories = State()
-    notes = State()
+    name = State(); duration = State(); calories = State(); notes = State()
